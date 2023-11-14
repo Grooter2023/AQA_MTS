2b3e573edd25c5a825d635856868d500fccada81
Tany Anisimova <grooter@mail.ru>
2023-11-14 20:08:51 +0300

    tmp1

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -1,5 +1,113 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Collections.Generic;
+/*
+{
+    // Это сообщение будет выведено на экран
+    /* Многострочный 
+     * вид
+     * клмментария
+      */
+/*
+    Console.WriteLine("Hello, World!");
+    {     
+        Console.WriteLine("It's a nice day!");
+    }
+    const int MINUTES_IN_HOUR = 60;
 
-Console.WriteLine("Hello, World!");
-First commit
+    string name;
+
+    name = "Bob";
+    Console.WriteLine(name);
+    
+    name = "Bob1";
+    Console.WriteLine(name);
+
+    name = "Bob2";
+    Console.WriteLine(name);
+
+    Console.WriteLine(true); // логические литералы
+    Console.WriteLine(false);// логические литералы
+
+
+    Console.WriteLine(-10); // целочисленные литералы
+    Console.WriteLine(0);// целочисленные литералы
+    Console.WriteLine(10);// целочисленные литералы
+    Console.WriteLine(0b10011);// целочисленные литералы
+
+
+    Console.WriteLine(1.24); // дробное
+    Console.WriteLine(1.24F);// число с дробной точкой вещественый
+
+    // Console.WriteLine(null); 
+    
+    Console.WriteLine("It's a \t nice day!");// \t - табуляция
+    Console.WriteLine("It's a \n nice day!");// \n - перенос на новую строку
+    Console.WriteLine("It's a \\ nice day!");// \\ указание слеша в тексте
+
+    bool flag = true;
+    flag = false;
+
+    sbyte sb = 100;
+
+    float a = 3.14F;
+    float a1 = 3.14F;
+
+    decimal c = 1005.5M;
+    decimal c1 = 1005.5M;
+
+    var d = 10;
+    var d1 = 10.2F;
+
+    int j;
+    j = 20;
+
+    var j1 = 20; // верное указание 
+}
+    */
+
+/* 
+Console.WriteLine("Добро пожаловать в С#");
+Console.Write("Надеюсь Вы ");
+Console.Write("постигните нюансы в С#! ");
+Console.WriteLine("И все будет супер гуд!!!");
+*\
+
+/*
+int j;
+j = 20;
+
+Console.WriteLine("J = " + j);// конкатинация!!
+
+string name = "Tom";
+int age = 34;
+double height = 1.7;
+
+Console.WriteLine($"Имя: {name} Возраст {age} Рост:{height}");Console.WriteLine("J = " + j);
+Console.WriteLine($"Имя: {0} Возраст {2} Рост:{1}, name , height, age"); Console.WriteLine("J = " + j);
+*/
+
+
+Console.WriteLine("Введите свое имя:");
+string? name = Console.ReadLine();
+Console.WriteLine($"Ваше имя: {name}");
+
+Console.WriteLine("Введите свой возраст:");
+string? age str = Console.ReadLine();
+int age = Convert.ToInt32(age_str);
+Console.WriteLine($"Ваш возраст: {age}");
+
+
+/* 
+//Решение нашей задачи
+// Подготовка данных
+
+bool isWhiteBreadFresh = true;
+ushort whiteBreadPrice = 100;
+ushort butterPrice = 230;
+ushort milkPrice = 170;
+float milkFatPercentage = 1.2F;
+}
+*/
+
+
+
