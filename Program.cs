e03472ae8a5cad0b8b22736693bd61b7cb7d1469
Tany Anisimova <grooter@mail.ru>
2024-01-13 20:58:24 +0300

    final comment on task one

--- Program.cs
+++ Program.cs
@@ -1,13 +1,34 @@
-﻿using Task_two;
+﻿using System;
 
-namespace Task_two
+namespace Task_one;
 
-public class Program
+class Program // т.к в явном виде не задан модификатор доступа, то он по умолчанию будет internal
 {
     static void Main()
     {
-        Patient patient = new("Генрих", 12);
+        // Создание массива фигур
+        Figure[] figures = new Figure[5]
+        {
+            new Triangle(3, 4, 5),
+            new Rectangle(6, 10),
+            new Circle(10),
+            new Triangle(6, 6, 6),
+            new Rectangle(7, 10)
+        }
 
-        patient.treatment();
+        // Для суммы периметров и площадей переменные
+        double Area = 0;
+        double Perimeter = 0;
+
+        // Вычисление общей площади и периметра
+        foreach (Figure figure in figures)
+        {
+            Area += figure.CalculationArea();
+            Perimeter += figure.CalculationPerimeter();
+        }
+
+        // Вывод на консоль суммы периметров и площади всех фигур в массиве
+        Console.WriteLine($"Общая площадь фигур: {Area}");
+        Console.WriteLine($"Общий периметр фигур: {Perimeter}");
     }
 }
