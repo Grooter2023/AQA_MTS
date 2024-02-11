fb167149c7876cf7afe41cf2a4ed68d3f765cc4c
Tany Anisimova <grooter@mail.ru>
2024-02-11 18:33:28 +0300

    FIRST COMMENT

---
+++ NUnitProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+namespace NUnitProject
+{
+    public class Tests  
+    {
+        [Test]
+        public void Test0()
+        {
+            Console.WriteLine($"{this}: Test...");
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
