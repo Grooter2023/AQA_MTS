40751e2e7a106bcc3f7f8e72ca26da8803aabc0d
Tatiana <150178488+Grooter2023@users.noreply.github.com>
2024-02-25 22:53:55 +0300

    final commit

---
+++ SauceDemo/Helpers/SeverityAttribute.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+namespace SauceDemo.Helpers;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class SeverityAttribute : PropertyAttribute
+    {
+        public SeverityAttribute(SeverityLevel level) : base(level.ToString()) { }
+    }
