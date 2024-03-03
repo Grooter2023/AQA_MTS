3282e36f2f25148723b538a4b52e4cba7b65bf9d
Tatiana <150178488+Grooter2023@users.noreply.github.com>
2024-03-03 15:02:32 +0300

    first commit

---
+++ SeleniumAdvanced/Pages/ProjectPages/AddProjectPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using SeleniumBasic.ProjectPages;
+
+namespace SeleniumBasic.Pages.ProjectPages;
+
+public class AddProjectPage : ProjectBasePage
+{
+    private static string END_POINT = "index.php?/admin/projects/add";
+
+    // Описание элементов
+    private static readonly By AddButtonBy = By.Id("name");
+
+
+    public AddProjectPage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    protected override string GetEndpoint()
+    {
+        return END_POINT;
+    }
+
+    public override bool IsPageOpened()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Атомарные Методы
+    public IWebElement AddButton => Driver.FindElement(AddButtonBy);
+}
---
+++ SeleniumAdvanced/Pages/ProjectPages/ProjectBasePage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using SeleniumBasic.Helpers;
+
+namespace SeleniumBasic.Pages.ProjectPages;
+
+public abstract class ProjectBasePage : BasePage
+{
+    private static readonly By NameInputBy = By.Id("name");
+
+    public ProjectBasePage(IWebDriver driver, bool openPageByUrl) : base(driver, openPageByUrl)
+    {
+    }
+
+    // Атомарные Методы
+    public IWebElement NameInput => WaitsHelper.WaitForExists(NameInputBy);
+}
